ac84831f6a080a92e319ac75011f37144a6d5bca
jungwon-moon <jwoung0@naver.com>
2021-09-14 17:04:26 +0900

    4-ch1

--- facebook/models.py
+++ facebook/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
-
-
 class Article(models.Model):
     author = models.CharField(max_length=120)
     title = models.CharField(max_length=120)
@@ -12,3 +11,12 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Page(models.Model):
+    master = models.CharField(max_length=120)
+    name = models.CharField(max_length=120)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
