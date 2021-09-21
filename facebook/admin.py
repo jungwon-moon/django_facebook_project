b69b98599442c634a012e5b172bbb497bac3172b
jungwon-moon <jwoung0@naver.com>
2021-09-21 16:16:12 +0900

    6-1

--- facebook/admin.py
+++ facebook/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from facebook.models import Article, Page
+from facebook.models import Article, Comment, Page
 
 admin.site.register(Article)
+admin.site.register(Comment)
 admin.site.register(Page)
